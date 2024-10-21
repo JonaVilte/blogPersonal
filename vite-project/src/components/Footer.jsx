952d2bcfd037835eec6423525f216d381c984d2c
Jonatan <togo36918@gmail.com>
2024-10-21 14:48:33 -0300

    Se agrego un link y un parrafo en el footer

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -1,3 +1,19 @@
 export default function Footer() {
-  return <footer>buenas, este es mi footer</footer>;
+  return <footer className="footer">
+  <div className="footer-container">
+    <div className="footer-logo">
+      <h3>Blog de Jonatan</h3>
+      <p>© 2024 - Todos los derechos reservados</p>
+    </div>
+
+
+    <div className="footer-social">
+      <a href="https://instagram.com" aria-label="Instagram" className="social-link">
+        <img src="./public/image/instagram.webp" alt="Instagram" />
+        <p className="">Instagram</p>
+      </a>
+    </div>
+  </div>
+</footer>
+
 }
